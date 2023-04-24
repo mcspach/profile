9e84ce9577e4c97b92f5a9eeeb573d5e61d8043b
Matthew Spach <matthews@anyfans.com>
2023-04-24 10:18:18 -0700

    add titles

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ function scrollFunction() {
   }
 }
 
-// animate cards
+// animate cards in
 const rowcards = document.querySelectorAll('.rowcard');
 const cards = [...rowcards, ...document.querySelectorAll('.colcard')];
 console.log(cards);
 fraction = .3;
 
-function checkScrollRowCard() {
+function checkScrollIn() {
   for (let i = 0; i < cards.length; i++) {
 
     let card = cards[i];
@@ -39,6 +39,33 @@ function checkScrollRowCard() {
   }
 }
 
-window.addEventListener('scroll', checkScrollRowCard, false);
-window.addEventListener('resize', checkScrollRowCard, false);
+window.addEventListener('scroll', checkScrollIn, false);
+window.addEventListener('resize', checkScrollIn, false);
 
+
+// animate cards out
+fractionOut = .3;
+
+function checkScrollOut() {
+  for (let i = 0; i < cards.length; i++) {
+
+    let card = cards[i];
+
+    let x = card.offsetLeft, y = card.offsetTop, w = card.offsetWidth, h = card.offsetHeight, r = x + w, //right
+      b = y + h, //bottom
+      visibleX, visibleY, visible;
+
+    visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+    visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+    visible = visibleX * visibleY / (w * h);
+
+    console.log(visible > fractionOut);
+    if (visible > fractionOut) {
+      card.classList.add('slideIn');
+    }
+  }
+}
+
+window.addEventListener('scroll', checkScrollOut, false);
+window.addEventListener('resize', checkScrollOut, false);
